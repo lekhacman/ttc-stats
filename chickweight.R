28b115777c2e88b668d4b439c937f8f316bdc91e
Le Khac Man <lekhacman@outlook.com>
2022-12-04 14:02:06 +0800

    lec1

---
+++ chickweight.R
@@ -0,0 +1,8 @@
+x = c(179, 160, 136, 227, 217, 168,108, 124, 143, 140)
+sum(x)
+
+completionTime = c(13.8, 14.1, 15.7, 14.5, 13.3, 14, 9, 15.1, 14.0)
+summary(completionTime)
+mad(completionTime)
+var(completionTime)
+sd(completionTime)
